a9bda33ad58c0b0c9d774341c3dfdacda32449af
Ardy Dwi Krisnanto <ardy.dwik@gmail.com>
2020-08-07 09:41:16 +0700

    file Migration

--- Pekan-3/Day- 2 Laravel/LaravelDasar/resources/views/master.blade.php
+++ Pekan-3/Day- 2 Laravel/LaravelDasar/resources/views/master.blade.php
@@ -14,6 +14,7 @@
   <!-- overlayScrollbars -->
   <link rel="stylesheet" href="{{asset('AdminLTE/dist/css/adminlte.min.css')}}">
   <link rel="stylesheet" href="{{asset('AdminLTE/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{asset('AdminLTE/plugins/datatables-bs4/css/dataTables.bootstrap4.css')}}">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
